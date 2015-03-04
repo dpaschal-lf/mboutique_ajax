64455c07246b4e49b0aa0891fbe3ddcc080c67c5
dpaschal <daniel.paschal@gmail.com>
2015-03-04 14:44:36 -0800

    added default and auto menu generation

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,6 +1,6 @@
 var pages={
     Welcome: 
-        {pageUrl : 'home.html'},
+        {pageUrl : 'home.html', default:true},
     "Our Macarons" : 
         { pageUrl : 'macarons.html'},
     "Gifts & Parties" : 
@@ -12,7 +12,7 @@ var pages={
 $("document").ready(function(){
 
 //do stuff when the document is ready!
-    $(".main-nav li a").each(function(){
+/*$(".main-nav li a").each(function(){
         //do something to each item found
         $(this).click(function(){
 
@@ -20,12 +20,31 @@ $("document").ready(function(){
             console.log(my_page);
             load_page(my_page.pageUrl);
         });
-
-    });
-    
+    });*/
+    create_menu();
 });
-
-
+/*<li data-url="home.html"><a >Welcome</a></li>*/
+function create_menu(){
+    var main_nav_ul = $(".main-nav ul");
+    for(var index in pages){
+        console.log("The index is "+index);
+        var li = $("<li/>");
+        var a = $("<a/>").text(index);
+        (function(){
+            var my_index = index;
+            li.on("click","a",function(){
+                var my_page = pages[my_index];
+                load_page(my_page.pageUrl);
+            });
+        })();
+        li.append(a);
+        //a.appendTo(li);
+        main_nav_ul.append(li);
+        if(pages[index].default==true){
+            load_page(pages[index].pageUrl);
+        }
+    }
+}
 
 
 function load_page(page_url){
@@ -35,3 +54,8 @@ function load_page(page_url){
     });
     
 }
+
+
+
+
+
